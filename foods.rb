152cba0b5317cd732c03c017e37eef4abe5fe089
jhsiaomei <jw1314@gmail.com>
2015-11-02 20:31:36 -0600

    counting to 10

---
+++ foods.rb
@@ -0,0 +1,13 @@
+foods = []
+​
+puts "Please enter your five favorite foods:"
+5.times do 
+  foods << gets.chomp
+end
+​
+count = 1
+​
+foods.each do |food|
+  puts count.to_s + ". #{food}"
+  count += 1
+end
---
+++ operators.rb
@@ -0,0 +1,5 @@
+count = 0
+while count < 11 do
+  puts count
+  count += 1
+end
