8638617fe066783e76d2d9d3591e1aa34795961d
jhsiaomei <jw1314@gmail.com>
2015-11-03 18:07:43 -0600

    adding banking file, instructor foods code

---
+++ banking.rb
@@ -0,0 +1,19 @@
+acct_info = []
+​
+5.times do 
+  puts "Enter first name:"
+  f_name = gets.chomp
+  puts "Enter last name:"
+  l_name = gets.chomp
+  puts "Enter e-mail:"
+  email = gets.chomp
+  person = { first_name: f_name, last_name: l_name, e_addy: email, acct_num: rand(1000000000..9999999999) }
+  acct_info << person
+end
+​
+acct_info.each do |person|
+  puts "FIRST NAME: " + person[:first_name]
+  puts "LAST NAME: " + person[:last_name]
+  puts "EMAIL: " + person[:e_addy]
+  puts "ACCT #: " + person[:acct_num].to_s
+end
--- foods.rb
+++ foods.rb
@@ -1,13 +1,37 @@
-foods = []
-​
-puts "Please enter your five favorite foods:"
-5.times do 
-  foods << gets.chomp
+# foods = []
+# ​
+# puts "Please enter your five favorite foods:"
+# 5.times do 
+#   foods << gets.chomp
+# end
+# ​
+# count = 1
+# ​
+# foods.each do |food|
+#   puts count.to_s + ". #{food}"
+#   count += 1
+# end
+
+
+
+puts "Enter your 5 favorite foods:"
+5.times do
+  food = gets.chomp
+  p food
 end
-​
-count = 1
-​
+
 foods.each do |food|
-  puts count.to_s + ". #{food}"
-  count += 1
+  p "I love #{food}"
 end
+
+foods.each_with_index do |food, index|
+  p "#{index + 1} food"
+end
+
+
+
+# foods = (1..5).map { gets.chomp }
+
+# foods.each_with_index do |food, index|
+#   p "#{index + 1} food"
+# end
